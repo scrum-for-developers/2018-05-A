16c2908476e540dd1abe35ba05764ac8fe0ec25c
Administrator <Administrator@raum08pc02>
2018-05-17 15:16:38 +0100

    SQ

--- worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
+++ worblehat-web/src/main/java/de/codecentric/psd/Worblehat.java
@@ -10,19 +10,19 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Worblehat {
 
 	public static void main(String[] args) {
-		try(Scanner scan = new Scanner(System.in)) {
-			ConfigurableApplicationContext applicationContext = SpringApplication.run(Worblehat.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(Worblehat.class, args);
+		Scanner scan = new Scanner(System.in);
 
-			// this code is basically to (a) demonstrate how to stop a Spring application and (b)
-			// get rid of the SonarQube warning to close the context properly
-			System.out.println("Enter 'stop' to stop Worblehat.");
+		// this code is basically to (a) demonstrate how to stop a Spring application and (b)
+		// get rid of the SonarQube warning to close the context properly
+		System.out.println("Enter 'stop' to stop Worblehat.");
 
-			String line;
-			do {
-				line = scan.nextLine();
-			} while (!"stop".equals(line));
-			applicationContext.close();
-		}
+		String line;
+		do {
+			line = scan.nextLine();
+		} while (!"stop".equals(line));
+		scan.close();
+		applicationContext.close();
 	}
 
 }
