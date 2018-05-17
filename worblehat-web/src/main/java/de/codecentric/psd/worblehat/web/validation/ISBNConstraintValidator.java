9ef02f0d781721a6b61af4a7ee382aa9c05be1ee
Volker <vk.github81.gmail.com>
2018-05-17 16:43:48 +0200

    ISBNVAL korr merge

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/validation/ISBNConstraintValidator.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/validation/ISBNConstraintValidator.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.codecentric.psd.worblehat.web.validation;
 
 import org.apache.commons.lang.StringUtils;
@@ -24,29 +23,3 @@ public class ISBNConstraintValidator implements ConstraintValidator<ISBN, String
 	}
 
 }
-=======
-package de.codecentric.psd.worblehat.web.validation;
-
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.validator.routines.ISBNValidator;
-
-public class ISBNConstraintValidator implements ConstraintValidator<ISBN, String> {
-
-	@Override
-	public void initialize(ISBN constraintAnnotation) {
-		// nop
-	}
-
-	@Override
-	public boolean isValid(String value, ConstraintValidatorContext context) {
-		// Don't validate null, empty and blank strings, since these are validated by @NotNull, @NotEmpty and @NotBlank
-		if(StringUtils.isNotBlank(value))
-			return ISBNValidator.getInstance().isValidISBN10(value);
-		return true;
-	}
-
-}
->>>>>>> 8a5d9703f11d8efb2dc84f2ea668c3d9fe35c502
